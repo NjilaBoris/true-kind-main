bc4f80c742d80dd950e9b965f2d4c8278954a05a
NjilaBoris <njilaboris2@gmail.com>
2026-02-01 14:02:16 +0100

    Setting Up Project

---
+++ constant/index.ts
@@ -0,0 +1,53 @@
+export const productsDetails = [
+  {
+    icon: "/icons/lense.svg",
+    name: "Radical Transparency",
+    details:
+      "No black boxes, nothing to hide, we disclose our full formulas, so you will never have to guess what's in it and how much.",
+  },
+  {
+    icon: "/icons/leaf.svg",
+    name: "Clean, Beyond Reproach",
+    details:
+      "Truly clean with only verified ingredients; and free from over 1800 questionable ingredients. Because what you put on your skin matters.",
+  },
+  {
+    icon: "/icons/icon-highest-standards.svg",
+    name: "Conscious & Responsible",
+    details:
+      "Peta Certified Vegan and Cruelty Free. Our products are always housed in responsible packaging and made sustainably.",
+  },
+  {
+    icon: "/icons/tube.svg",
+    name: "Potent & Multi Tasking",
+    details:
+      "Our formulas are chock-a-block with actives, anti oxidants, skin restoring agents backed by dermal science that aim to deliver real results.",
+  },
+];
+
+export const productsData1 = [
+  {
+    type: "Pure brilliance",
+    icon: "/icons/cart.svg",
+    image: "/images/ZurDj7VsGrYSvh0W_1-Photoroom.png",
+    heading1: "AHA Brightening Exfoliant",
+    heading2: " Cleanser/Face Wash",
+    price: " ₹899 ",
+  },
+  {
+    type: "Pure brilliance",
+    icon: "/icons/cart.svg",
+    image: "/images/ZurL27VsGrYSviVl_1-Photoroom.png",
+    heading1: "Bio Exfoliant Brightening ",
+    heading2: "Sleeping Mask",
+    price: " ₹899 ",
+  },
+  {
+    type: "Pure brilliance",
+    icon: "/icons/cart.svg",
+    image: "/images/ZurMbbVsGrYSviXa_1-Photoroom.png",
+    heading1: "AHA Brightening Exfoliant",
+    heading2: " Cleanser/Face Wash",
+    price: " ₹899 ",
+  },
+];
---
+++ icons/index.tsx
@@ -0,0 +1,309 @@
+export const ArrowDownIcon = (props: React.SVGAttributes<SVGSVGElement>) => {
+  <svg
+    data-v-9ff11d95=""
+    className="arrow-text"
+    width="260"
+    height="568"
+    viewBox="0 0 260 568"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <mask data-v-9ff11d95="" id="maskarrowtext_blog">
+      <path
+        data-v-9ff11d95=""
+        className="mask-path-intro"
+        stroke-dashoffset="835"
+        stroke-dasharray="835, 850"
+        data-duration="1.4"
+        d="M0.271018 563.471C0.0722111 563.702 0.0982756 564.051 0.329254 564.249L4.09319 567.489C4.32415 567.688 4.67257 567.662 4.8714 567.431C5.0702 567.2 5.04412 566.851 4.81314 566.653L1.46741 563.773L4.3472 560.427C4.54599 560.196 4.5199 559.848 4.28894 559.649C4.05796 559.45 3.70955 559.476 3.51074 559.707L0.271018 563.471ZM31.6107 1.53591C212.353 101.883 273.377 237.214 254.913 349.923C236.447 462.65 138.442 552.968 0.648044 563.281L0.730409 564.381C138.975 554.034 237.444 463.386 256.002 350.101C274.563 236.797 213.162 101.07 32.1464 0.571044L31.6107 1.53591Z"
+        style="stroke-dashoffset: 834.9; stroke-dasharray: 835px, 792.739px;"
+      ></path>
+    </mask>
+    <path
+      data-v-9ff11d95=""
+      mask="url(#maskarrowtext_blog)"
+      d="M0.271018 563.471C0.0722111 563.702 0.0982756 564.051 0.329254 564.249L4.09319 567.489C4.32415 567.688 4.67257 567.662 4.8714 567.431C5.0702 567.2 5.04412 566.851 4.81314 566.653L1.46741 563.773L4.3472 560.427C4.54599 560.196 4.5199 559.848 4.28894 559.649C4.05796 559.45 3.70955 559.476 3.51074 559.707L0.271018 563.471ZM31.6107 1.53591C212.353 101.883 273.377 237.214 254.913 349.923C236.447 462.65 138.442 552.968 0.648044 563.281L0.730409 564.381C138.975 554.034 237.444 463.386 256.002 350.101C274.563 236.797 213.162 101.07 32.1464 0.571044L31.6107 1.53591Z"
+      fill="black"
+    ></path>
+  </svg>;
+};
+export const StarIcon = (props: React.SVGAttributes<SVGSVGElement>) => {
+  return (
+    <svg
+      className="icon-1"
+      width="33"
+      height="33"
+      viewBox="0 0 33 33"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      data-v-90168d66=""
+      {...props}
+    >
+      <path
+        d="M16.5 0C16.5 0 17.9497 8.10256 21.4236 11.5764C24.8974 15.0503 33 16.5 33 16.5C33 16.5 24.8974 17.9497 21.4236 21.4236C17.9497 24.8974 16.5 33 16.5 33C16.5 33 15.0503 24.8974 11.5764 21.4236C8.10256 17.9497 0 16.5 0 16.5C0 16.5 8.10256 15.0503 11.5764 11.5764C15.0503 8.10256 16.5 0 16.5 0Z"
+        fill="white"
+        data-v-90168d66=""
+      ></path>
+    </svg>
+  );
+};
+
+export const LineArrow = (props: React.SVGAttributes<SVGSVGElement>) => {
+  return (
+    <svg
+      className="icon-2"
+      width="283"
+      height="283"
+      viewBox="0 0 283 283"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      data-v-90168d66=""
+      {...props}
+    >
+      <line
+        x1="0.646447"
+        y1="282.646"
+        x2="282.646"
+        y2="0.646449"
+        stroke="#D8D8D8"
+        data-v-90168d66=""
+      ></line>
+    </svg>
+  );
+};
+
+export const ArrowForwardOutline = (
+  props: React.SVGAttributes<SVGSVGElement>
+) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="ionicon"
+      viewBox="0 0 512 512"
+      {...props}
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="48"
+        d="M268 112l144 144-144 144M392 256H100"
+      />
+    </svg>
+  );
+};
+export const ArrowRightDark = (props: React.SVGAttributes<SVGSVGElement>) => {
+  return (
+    <svg
+      viewBox="-2.4 -2.4 28.80 28.80"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="#000000"
+      strokeWidth="0.00024000000000000003"
+      transform="rotate(-45)matrix(1, 0, 0, -1, 0, 0)"
+      {...props}
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <path
+          d="M13.2328 16.4569C12.9328 16.7426 12.9212 17.2173 13.2069 17.5172C13.4926 17.8172 13.9673 17.8288 14.2672 17.5431L13.2328 16.4569ZM19.5172 12.5431C19.8172 12.2574 19.8288 11.7827 19.5431 11.4828C19.2574 11.1828 18.7827 11.1712 18.4828 11.4569L19.5172 12.5431ZM18.4828 12.5431C18.7827 12.8288 19.2574 12.8172 19.5431 12.5172C19.8288 12.2173 19.8172 11.7426 19.5172 11.4569L18.4828 12.5431ZM14.2672 6.4569C13.9673 6.17123 13.4926 6.18281 13.2069 6.48276C12.9212 6.78271 12.9328 7.25744 13.2328 7.5431L14.2672 6.4569ZM19 12.75C19.4142 12.75 19.75 12.4142 19.75 12C19.75 11.5858 19.4142 11.25 19 11.25V12.75ZM5 11.25C4.58579 11.25 4.25 11.5858 4.25 12C4.25 12.4142 4.58579 12.75 5 12.75V11.25ZM14.2672 17.5431L19.5172 12.5431L18.4828 11.4569L13.2328 16.4569L14.2672 17.5431ZM19.5172 11.4569L14.2672 6.4569L13.2328 7.5431L18.4828 12.5431L19.5172 11.4569ZM19 11.25L5 11.25V12.75L19 12.75V11.25Z"
+          fill="#000000"
+        ></path>{" "}
+      </g>
+    </svg>
+  );
+};
+
+export const ArrowRight = (props: React.SVGAttributes<SVGSVGElement>) => {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      transform="rotate(180)"
+      stroke="#ffffff"
+      {...props}
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <path
+          d="M6 12H18M6 12L11 7M6 12L11 17"
+          stroke="#fff"
+          strokeWidth="0.40800000000000003"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>{" "}
+      </g>
+    </svg>
+  );
+};
+export const UprightWhiteArrow = (
+  props: React.SVGAttributes<SVGSVGElement>
+) => {
+  return (
+    <svg
+      viewBox="-2.4 -2.4 28.80 28.80"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="#fff"
+      strokeWidth="0.00024000000000000003"
+      transform="rotate(-45)matrix(1, 0, 0, -1, 0, 0)"
+      {...props}
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <path
+          d="M13.2328 16.4569C12.9328 16.7426 12.9212 17.2173 13.2069 17.5172C13.4926 17.8172 13.9673 17.8288 14.2672 17.5431L13.2328 16.4569ZM19.5172 12.5431C19.8172 12.2574 19.8288 11.7827 19.5431 11.4828C19.2574 11.1828 18.7827 11.1712 18.4828 11.4569L19.5172 12.5431ZM18.4828 12.5431C18.7827 12.8288 19.2574 12.8172 19.5431 12.5172C19.8288 12.2173 19.8172 11.7426 19.5172 11.4569L18.4828 12.5431ZM14.2672 6.4569C13.9673 6.17123 13.4926 6.18281 13.2069 6.48276C12.9212 6.78271 12.9328 7.25744 13.2328 7.5431L14.2672 6.4569ZM19 12.75C19.4142 12.75 19.75 12.4142 19.75 12C19.75 11.5858 19.4142 11.25 19 11.25V12.75ZM5 11.25C4.58579 11.25 4.25 11.5858 4.25 12C4.25 12.4142 4.58579 12.75 5 12.75V11.25ZM14.2672 17.5431L19.5172 12.5431L18.4828 11.4569L13.2328 16.4569L14.2672 17.5431ZM19.5172 11.4569L14.2672 6.4569L13.2328 7.5431L18.4828 12.5431L19.5172 11.4569ZM19 11.25L5 11.25V12.75L19 12.75V11.25Z"
+          fill="#fff"
+        ></path>{" "}
+      </g>
+    </svg>
+  );
+};
+
+export const CartIcon = (props: React.SVGAttributes<SVGSVGElement>) => {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <path
+          d="M8 11.01V11M16 11.01V11M8 8V7C8 5.93913 8.42143 4.92172 9.17157 4.17157C9.92172 3.42143 10.9391 3 12 3C13.0609 3 14.0783 3.42143 14.8284 4.17157C15.5786 4.92172 16 5.93913 16 7V8M8 8H6.84027C5.80009 8 4.93356 8.79732 4.84718 9.83391L4.18051 17.8339C4.08334 18.9999 5.00352 20 6.1736 20H17.8264C18.9965 20 19.9167 18.9999 19.8195 17.8339L19.1528 9.83391C19.0664 8.79732 18.1999 8 17.1597 8H16M8 8H16"
+          stroke="#000000"
+          strokeWidth="0.192"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>{" "}
+      </g>
+    </svg>
+  );
+};
+
+export const TubeIcon = (props: React.SVGAttributes<SVGSVGElement>) => {
+  return (
+    <svg
+      fill="#000000"
+      viewBox="0 0 256 256"
+      id="Flat"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="#000000"
+      strokeWidth="0.00256"
+      {...props}
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <path d="M234.82812,89.17139l-60-60a4.00088,4.00088,0,0,0-5.65673,0l-130,130a40.76965,40.76965,0,0,0,57.65673,57.65722L210.16064,103.49609l23.104-7.70117a3.99994,3.99994,0,0,0,1.56347-6.62353Zm-143.65673,122a32.76947,32.76947,0,0,1-46.34327-46.34278l25.81055-25.81018c1.91455-1.42053,19.51026-13.4718,47.57227.55921,11.33886,5.66943,21.28613,7.57568,29.542,7.57519a51.39,51.39,0,0,0,8.07568-.63861Zm115.564-114.96631a4.00871,4.00871,0,0,0-1.564.96631L169.36084,132.982c-1.91748,1.42249-19.51221,13.47052-47.57178-.55963-15.335-7.66729-28.11914-8.44354-37.61767-6.9367L172,37.65674l52.59521,52.59521Z"></path>{" "}
+      </g>
+    </svg>
+  );
+};
+
+export const LeafIcon = (props: React.SVGAttributes<SVGSVGElement>) => {
+  return (
+    <svg
+      width="212px"
+      height="212px"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      transform="rotate(0)matrix(1, 0, 0, 1, 0, 0)"
+      {...props}
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <path
+          d="M4.44893 17.009C-0.246384 7.83762 7.34051 0.686125 19.5546 3.61245C20.416 3.81881 21.0081 4.60984 20.965 5.49452C20.5862 13.288 17.0341 17.7048 6.13252 17.9857C5.43022 18.0038 4.76908 17.6344 4.44893 17.009Z"
+          stroke="#323232"
+          strokeWidth="0.21600000000000003"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></path>{" "}
+        <path
+          d="M3.99999 21C5.50005 15.5 6 12.5 12 9.99997"
+          stroke="#323232"
+          strokeWidth="0.21600000000000003"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>{" "}
+      </g>
+    </svg>
+  );
+};
+
+export const LenseIcon = (props: React.SVGAttributes<SVGSVGElement>) => {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <path
+          d="M19.9604 11.4802C19.9604 13.8094 19.0227 15.9176 17.5019 17.4512C16.9332 18.0247 16.2834 18.5173 15.5716 18.9102C14.3594 19.5793 12.9658 19.9604 11.4802 19.9604C6.79672 19.9604 3 16.1637 3 11.4802C3 6.79672 6.79672 3 11.4802 3C16.1637 3 19.9604 6.79672 19.9604 11.4802Z"
+          stroke="#333333"
+          stroke-width="0.192"
+        ></path>{" "}
+        <path
+          d="M18.1553 18.1553L21.8871 21.8871"
+          stroke="#333333"
+          stroke-width="0.192"
+          stroke-linecap="round"
+        ></path>{" "}
+      </g>
+    </svg>
+  );
+};
+
+export const HighestStandardIcon = (
+  props: React.SVGAttributes<SVGSVGElement>
+) => {};
